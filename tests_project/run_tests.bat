110da41c7e8fecb8c21edede63aba7481dc0aed7
Kokoszsz <95125887+Kokoszsz@users.noreply.github.com>
2023-12-01 16:48:17 +0100

    dsada

--- tests_project/run_tests.bat
+++ tests_project/run_tests.bat
@@ -1,28 +1,38 @@
 @echo off
 cls
 
-REM Define array for test case folder names
-set "test_case_folders=test_case_1 test_case_2 test_case_3"
 
-REM Loop over test case folders
-for %%i in (%test_case_folders%) do (
-    set "current_folder=tests_project\%%i"
+set input_file=tests_project\test_case_1\test_data.txt
+set output_file=tests_project\test_case_1\test_output.txt
+set expected_output_file=tests_project\test_case_1\test_expected_output.txt
 
-    set "input_file=!current_folder!\test_data.txt"
-    set "output_file=!current_folder!\test_output.txt"
-    set "expected_output_file=!current_folder!\test_expected_output.txt"
+x64\Debug\project.exe -g %input_file% -c %output_file%
 
-    x64\Debug\project.exe -g "!input_file!" -c "!output_file!"
+echo Test 1 failed.
 
-    fc "!output_file!" "!expected_output_file!" > nul
-    if errorlevel 1 (
-        echo Tests for !current_folder! failed.
-        pause
-        exit 1
-    ) else (
-        echo Tests for !current_folder! passed.
-    )
+set input_file=tests_project\test_case_2\test_data.txt
+set output_file=tests_project\test_case_2\test_output.txt
+set expected_output_file=tests_project\test_case_2\test_expected_output.txt
+
+x64\Debug\project.exe -g %input_file% -c %output_file%
+
+echo Test 2 failed.
+
+set input_file=tests_project\test_case_3\test_data.txt
+set output_file=tests_project\test_case_3\test_output.txt
+set expected_output_file=tests_project\test_case_3\test_expected_output.txt
+
+x64\Debug\project.exe -g %input_file% -c %output_file%
+
+echo Test 3 failed.
+
+fc %output_file% %expected_output_file% > nul
+if errorlevel 1 (
+    echo Tests failed.
+    pause
+    exit 1
+) else (
+    echo Tests passed.
+    pause
+    exit 0
 )
-
-pause
-exit 0
