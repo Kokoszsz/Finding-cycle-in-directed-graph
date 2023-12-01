9bb49ae708cac7c63b5ec87fb106c34d5e7894c6
Kokoszsz <95125887+Kokoszsz@users.noreply.github.com>
2023-12-01 08:37:01 +0100

    change runner form windows to linux

--- run_tests.bat
+++ run_tests.bat
@@ -1,22 +1,19 @@
-@echo off
-cls
+#!/bin/bash
+clear
 
-REM Define input and output file names
-set input_file=tests_project\test_data_1.txt
-set output_file=tests_project\test_output_1.txt
-set expected_output_file=tests_project\test_expected_output_1.txt
+# Define input and output file names
+input_file=tests_project/test_data_1.txt
+output_file=tests_project/test_output_1.txt
+expected_output_file=tests_project/test_expected_output_1.txt
 
-REM Run your program with command-line arguments
-x64\Debug\project.exe -g %input_file% -c %output_file%
+# Run your program with command-line arguments
+./x64/Debug/project.exe -g "$input_file" -c "$output_file"
 
-REM Compare actual and expected output
-fc %output_file% %expected_output_file% > nul
-if errorlevel 1 (
-    echo Tests failed.
-    pause
-    exit 1
-) else (
-    echo Tests passed.
-    pause
+# Compare actual and expected output
+if cmp -s "$output_file" "$expected_output_file"; then
+    echo "Tests passed."
     exit 0
-)
+else
+    echo "Tests failed."
+    exit 1
+fi
