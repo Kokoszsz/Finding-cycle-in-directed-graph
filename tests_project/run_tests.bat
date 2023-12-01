f1e03d9f9907404932743a707787e54997561e37
Kokoszsz <95125887+Kokoszsz@users.noreply.github.com>
2023-12-01 16:42:15 +0100

    Update run_tests.bat

--- tests_project/run_tests.bat
+++ tests_project/run_tests.bat
@@ -1,18 +1,20 @@
 @echo off
 cls
 
-set test_case_folders=("test_case_1" "test_case_2" "test_case_3")
+REM Define array for test case folder names
+set "test_case_folders=test_case_1 test_case_2 test_case_3"
 
+REM Loop over test case folders
 for %%i in (%test_case_folders%) do (
-    set current_folder=tests_project\%%i
+    set "current_folder=tests_project\%%i"
 
-    set input_file=!current_folder!\test_data.txt
-    set output_file=!current_folder!\test_output.txt
-    set expected_output_file=!current_folder!\test_expected_output.txt
+    set "input_file=!current_folder!\test_data.txt"
+    set "output_file=!current_folder!\test_output.txt"
+    set "expected_output_file=!current_folder!\test_expected_output.txt"
 
-    x64\Debug\project.exe -g !input_file! -c !output_file!
+    x64\Debug\project.exe -g "!input_file!" -c "!output_file!"
 
-    fc !output_file! !expected_output_file! > nul
+    fc "!output_file!" "!expected_output_file!" > nul
     if errorlevel 1 (
         echo Tests for !current_folder! failed.
         pause
