24b02cb5dba5a4269001a1cb3a95638be3f930e9
Kokoszsz <95125887+Kokoszsz@users.noreply.github.com>
2023-12-01 09:58:06 +0100

    now it should not

--- run_tests.sh
+++ run_tests.sh
@@ -6,7 +6,8 @@ input_file=tests_project/test_data_1.txt
 output_file=tests_project/test_output_1.txt
 expected_output_file=tests_project/test_expected_output_1.txt
 
-
+# Run your program with command-line arguments
+./x64/Debug/project.exe -g "$input_file" -c "$output_file"
 
 # Compare actual and expected output
 if cmp -s "$output_file" "$expected_output_file"; then
