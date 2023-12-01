29d57927a6662ae4e07edae40a0b86316de80694
Kokoszsz <95125887+Kokoszsz@users.noreply.github.com>
2023-12-01 16:29:54 +0100

    testiiing

--- tests_project/run_tests.bat
+++ tests_project/run_tests.bat
@@ -1,22 +1,26 @@
 @echo off
 cls
 
-REM Define input and output file names
-set input_file=tests_project\test_case_1\test_data_1.txt
-set output_file=tests_project\test_case_1\test_output_1.txt
-set expected_output_file=tests_project\test_case_1\test_expected_output_1.txt
+set test_case_folders=("test_case_1" "test_case_2" "test_case_3")
 
-REM Run your program with command-line arguments
-x64\Debug\project.exe -g %input_file% -c %output_file%
+for %%i in (%test_case_folders%) do (
+    set current_folder=tests_project\%%i
 
-REM Compare actual and expected output
-fc %output_file% %expected_output_file% > nul
-if errorlevel 1 (
-    echo Tests failed.
-    pause
-    exit 1
-) else (
-    echo Tests passed.
-    pause
-    exit 0
+    set input_file=!current_folder!\test_data.txt
+    set output_file=!current_folder!\test_output.txt
+    set expected_output_file=!current_folder!\test_expected_output.txt
+
+    x64\Debug\project.exe -g !input_file! -c !output_file!
+
+    fc !output_file! !expected_output_file! > nul
+    if errorlevel 1 (
+        echo Tests for !current_folder! failed.
+        pause
+        exit 1
+    ) else (
+        echo Tests for !current_folder! passed.
+    )
 )
+
+pause
+exit 0
