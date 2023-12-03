36b17f5685643a3ec18a9f71e6e64d0e601d231e
Kokoszsz <95125887+Kokoszsz@users.noreply.github.com>
2023-12-03 01:11:36 +0100

    Update run_tests.bat

--- project/tests_project/run_tests.bat
+++ project/tests_project/run_tests.bat
@@ -2,8 +2,10 @@
 cls
 
 setlocal enabledelayedexpansion
-
+echo Current directory: %cd%
 for /l %%i in (1,1,8) do (
+
+	
     set input_file=tests_project\test_case_%%i\test_data.txt
     set output_file=tests_project\test_case_%%i\test_output.txt
     set expected_output_file=tests_project\test_case_%%i\test_expected_output.txt
