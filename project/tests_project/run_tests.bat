cd14f542c9f2cd338480b84ac3e4f436ff2c1d46
Kokoszsz <95125887+Kokoszsz@users.noreply.github.com>
2023-12-03 02:48:39 +0100

    Update run_tests.bat

--- project/tests_project/run_tests.bat
+++ project/tests_project/run_tests.bat
@@ -12,7 +12,7 @@ for /l %%i in (1,1,9) do (
 	type nul > project\tests_project\test_case_%%i\test_output.txt
 	set output_file=project\tests_project\test_case_%%i\test_output.txt
 
-    x64\Debug\project.exe -g !input_file! -c !output_file!
+    project\x64\Debug\project.exe -g !input_file! -c !output_file!
 
 
     fc !output_file! !expected_output_file! > nul
