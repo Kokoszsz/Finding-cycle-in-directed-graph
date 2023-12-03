c1800cb930da18870168498ac1b1e0e35403a5e8
Kokoszsz <95125887+Kokoszsz@users.noreply.github.com>
2023-12-03 02:10:05 +0100

    Update run_tests.bat

--- project/tests_project/run_tests.bat
+++ project/tests_project/run_tests.bat
@@ -6,14 +6,13 @@ echo Current directory: %cd%
 for /l %%i in (1,1,8) do (
 
 	
-    set input_file=tests_project\test_case_%%i\test_data.txt
-    set expected_output_file=tests_project\test_case_%%i\test_expected_output.txt
+    set input_file=project\tests_project\test_case_%%i\test_data.txt
+    set expected_output_file=project\tests_project\test_case_%%i\test_expected_output.txt
 
 	type nul > tests_project\test_case_%%i\test_output.txt
 
     x64\Debug\project.exe -g !input_file! -c !output_file!
 
-    echo Test %%i failed.
 
     fc !output_file! !expected_output_file! > nul
     if errorlevel 1 (
