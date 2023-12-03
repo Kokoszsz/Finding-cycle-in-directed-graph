c3d4ddf3f0be5a7d29da2718c873e526e01b44b7
Kokoszsz <95125887+Kokoszsz@users.noreply.github.com>
2023-12-03 01:06:49 +0100

    Update run_tests.bat

--- project/tests_project/run_tests.bat
+++ project/tests_project/run_tests.bat
@@ -4,9 +4,11 @@ cls
 setlocal enabledelayedexpansion
 
 for /l %%i in (1,1,8) do (
-    set input_file=project\tests_project\test_case_%%i\test_data.txt
-    set output_file=project\tests_project\test_case_%%i\test_output.txt
-    set expected_output_file=project\tests_project\test_case_%%i\test_expected_output.txt
+    set input_file=tests_project\test_case_%%i\test_data.txt
+    set output_file=tests_project\test_case_%%i\test_output.txt
+    set expected_output_file=tests_project\test_case_%%i\test_expected_output.txt
+
+	type nul > !output_file!
 
     x64\Debug\project.exe -g !input_file! -c !output_file!
 
